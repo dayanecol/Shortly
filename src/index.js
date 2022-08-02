7d2e27e5f29ab91088c247d219b5fe4efe4f61ed
Dayane <dayanecarolina.oliveira@gmail.com>
2022-08-01 22:18:55 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express, {json} from "express";
+import cors from "cors";
+import chalk from "chalk";
+import dotenv from "dotenv";
+import router from "./routes/router.js";
+import db from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+app.use(router);
+
+app.get("/",(req,res)=>{
+    res.send("Funcionando!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(chalk.bold.green(`Servidor conectado na porta ${PORT}.`));
+});
