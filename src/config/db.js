9ff6225a11b00b60464db78569a88dce7e5e7eca
Dayane <dayanecarolina.oliveira@gmail.com>
2022-08-05 23:59:15 -0300

    update

--- src/config/db.js
+++ src/config/db.js
@@ -5,12 +5,15 @@ dotenv.config();
 const { Pool } = pg;
 
 const databaseConfig = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+  connectionString: process.env.DATABASE_URL
+};
+
+if(process.env.MODE === "PROD"){
+  databaseConfig.ssl = {
+    rejectUnauthorized: false
+  }
 }
 
 const db = new Pool(databaseConfig);
 
-export default db;
+export default db; 
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,45 +7,31 @@ import { v4 as uuid } from 'uuid';
 
 export async function signUp(req,res){
     const {name,email,password} = req.body;
-    const repeatEmail = db.query(
-        `SELECT * FROM users WHERE email = $1 `, [email]
-    );
-    if (repeatEmail.rowCount>0){
-        res.status(409).send("E-mail already registered");
+    
+    try {
+        const repeatEmail = db.query(
+            `SELECT * FROM users WHERE email = $1 `, [email]
+        );
+        if (repeatEmail.rowCount>0){
+            res.status(409).send("E-mail already registered");
+            return;
+        }
+        const hashPassword = bcrypt.hashSync(password, 10);
+
+        await db.query(
+            `INSERT INTO users (name, email, password)
+            VALUES ($1,$2,$3)`,[name,email,hashPassword]
+        );
+        res.sendStatus(201);
         return;
-    }
-    const hashPassword = bcrypt.hashSync(password, 10);
-
-    await db.query(
-        `INSERT INTO users (name, email, password)
-        VALUES ($1,$2,$3)`,[name,email,hashPassword]
-    );
-    res.sendStatus(201);
-    return;
-    // try {
-    //     const repeatEmail = db.query(
-    //         `SELECT * FROM users WHERE email = $1 `, [email]
-    //     );
-    //     if (repeatEmail.rowCount>0){
-    //         res.status(409).send("E-mail already registered");
-    //         return;
-    //     }
-    //     const hashPassword = bcrypt.hashSync(password, 10);
-
-    //     await db.query(
-    //         `INSERT INTO users (name, email, password)
-    //         VALUES ($1,$2,$3)`,[name,email,hashPassword]
-    //     );
-    //     res.sendStatus(201);
-    //     return;
             
-    // } catch (error) {
-    //     console.log(chalk.bold.red("Erro no servidor!"));
-    //     res.status(500).send({
-    //       message: "Internal server error while register user!",
-    //     });
-    //     return;    
-    // }
+    } catch (error) {
+        console.log(chalk.bold.red("Erro no servidor!"));
+        res.status(500).send({
+          message: "Internal server error while register user!",
+        });
+        return;    
+    }
 }
 
 export async function signIn(req,res){
